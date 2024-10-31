8c9cfb9584f660e994a8b349bd7ed7dbf886b911
ElkanaRozental <rozentlen@gmail.com>
2024-10-31 16:05:44 +0200

    fix the queries

--- app/repository/mission_repository.py
+++ app/repository/mission_repository.py
@@ -4,7 +4,7 @@ from returns.maybe import Maybe, Nothing, Some
 from returns.result import Result, Success, Failure
 
 from app.db.database import session_maker
-from app.models import Mission, Target, Country, City
+from app.models import Mission, Target, Country, City, TargetType
 
 
 def get_mission_by_id(mission_id) -> Maybe[Mission]:
@@ -13,32 +13,42 @@ def get_mission_by_id(mission_id) -> Maybe[Mission]:
         return Some(mission) if mission else Nothing
 
 
-def get_mission_between_date(start_date, end_date) -> List[Maybe[Mission]]:
+def get_mission_type_name(target_type) -> List[Mission]:
+    with session_maker() as session:
+        missions = (session.query(Mission)
+                    .join(Mission.targets)
+                    .join(Target.target_type)
+                    .filter(TargetType.target_type_name == target_type)
+                    .all())
+        return missions
+
+
+def get_mission_between_date(start_date, end_date) -> List[Mission]:
     with session_maker() as session:
         missions = (session.query(Mission)
                     .filter(start_date <= Mission.mission_date <= end_date)
                     .all())
-        return Some(missions) if missions else Nothing
+        return missions
 
 
-def get_mission_by_country_name(country_name):
+def get_mission_by_country_name(country_name) -> List[Mission]:
     with session_maker() as session:
-        missions = (session.query(Mission, Target, City, Country)
-                    .join(Target, Mission.mission_id == Target.mission_id)
-                    .join(City, City.city_id == Target.city_id)
-                    .join(Country, Country.country_id == City.country_id)
+        missions = (session.query(Mission)
+                    .join(Mission.targets)
+                    .join(Target.city)
+                    .join(City.country)
                     .filter(Country.country_name == country_name)
                     .all())
-        return Some(missions) if missions else Nothing
+        return missions
 
 
-def get_mission_by_target_industry(target_industry) -> List[Maybe[Mission]]:
+def get_mission_by_target_industry(target_industry) -> List[Mission]:
     with session_maker() as session:
-        missions = (session.query(Mission, Target)
-                    .join(Target, Target.mission_id == Mission.mission_id)
+        missions = (session.query(Mission)
+                    .join(Mission.targets)
                     .filter(Target.target_industry == target_industry)
                     .all())
-        return Some(missions) if missions else Nothing
+        return missions
 
 
 def create_mission(mission: Mission) -> Result[Mission, str]:
--- app/repository/target_repository.py
+++ app/repository/target_repository.py
@@ -1,20 +1,8 @@
-from typing import List
 
-from returns.maybe import Maybe, Some, Nothing
 from returns.result import Result, Success, Failure
 
 from app.db.database import session_maker
-from app.models import TargetType, Mission, Target
-
-
-def get_plains_by_target_type_name(target_type) -> List[Maybe[TargetType]]:
-    with session_maker() as session:
-        missions = (session.query(TargetType, Mission, Target)
-                    .join(Target, TargetType.target_type_id == Target.target_type_id)
-                    .join(Mission, Mission.mission_id == Target.mission_id)
-                    .filter(TargetType.target_type_name == target_type)
-                    .all())
-        return Some(missions) if missions else Nothing
+from app.models import Target
 
 
 def create_target(target: Target) -> Result[Target, str]:
